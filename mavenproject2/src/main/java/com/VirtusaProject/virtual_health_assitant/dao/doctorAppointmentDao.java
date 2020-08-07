c54c2b56d366c4a736875135591c089e2e1dc4d8
DELL <vickramark54@gmail.com>
2020-08-07 19:47:16 +0530

    Updating some features

--- mavenproject2/src/main/java/com/VirtusaProject/virtual_health_assitant/dao/doctorAppointmentDao.java
+++ mavenproject2/src/main/java/com/VirtusaProject/virtual_health_assitant/dao/doctorAppointmentDao.java
@@ -51,4 +51,36 @@ public class doctorAppointmentDao {
         }
         return result;
     }
+    public String getConfirmedPatient(doctorAppointmentSetter dm) throws SQLException
+    {
+       String result="";
+       String email=dm.getEmail();
+        dataBaseConnection db=new dataBaseConnection();
+        Connection con=db.getConnection();
+        String query="select doctor_id from doctor_details where email=?";
+        PreparedStatement pre =con.prepareStatement(query);
+        pre.setString(1, email);
+        ResultSet res=pre.executeQuery();
+        int id=0;
+        if(res.next())
+        {
+            id=res.getInt("doctor_id");
+        }
+        String query1="select patient_details.first_name,patient_details.email,patient_details.phone_number,patient_details.address,patient_details.place_name,pateint_doctor.prescriptionId,pateint_doctor.appointmentDate,pateint_doctor.appointmentReason,pateint_doctor.appointmentTime,pateint_doctor.confirmation from patient_details join pateint_doctor on pateint_doctor.patientId=patient_details.pateintId join doctor_details on doctor_details.doctor_id=pateint_doctor.doctorId where pateint_doctor.doctorId=? and pateint_doctor.confirmation=?;";
+        PreparedStatement pre1=con.prepareStatement(query1);
+        pre1.setInt(1, id);
+        pre1.setString(2,"Yes");
+        ResultSet res1=pre1.executeQuery();
+        int count=0;
+        if(res1.next())
+        {
+                    count=1;
+            result=res1.getString("patient_details.first_name")+","+res1.getString("patient_details.email")+","+res1.getString("patient_details.phone_number")+","+res1.getString("patient_details.address")+","+res1.getString("patient_details.place_name")+","+res1.getString("pateint_doctor.prescriptionId")+","+res1.getString("pateint_doctor.appointmentDate")+","+res1.getString("pateint_doctor.appointmentReason")+","+res1.getString("pateint_doctor.appointmentTime")+","+res1.getString("pateint_doctor.confirmation")+"|";
+            while(res1.next())
+            {
+                result=result+res1.getString("patient_details.first_name")+","+res1.getString("patient_details.email")+","+res1.getString("patient_details.phone_number")+","+res1.getString("patient_details.address")+","+res1.getString("patient_details.place_name")+","+res1.getString("pateint_doctor.prescriptionId")+","+res1.getString("pateint_doctor.appointmentDate")+","+res1.getString("pateint_doctor.appointmentReason")+","+res1.getString("pateint_doctor.appointmentTime")+","+res1.getString("pateint_doctor.confirmation")+"|";
+            }
+        }
+        return result;
+    }
 }
